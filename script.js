c7dd11ae70001bf813893a9ec5aceb2996e68486
Matias Vazquez <mativazquezfotografia@hotmail.com>
2023-12-08 18:03:23 -0300

    modif cursos

---
+++ script.js
@@ -0,0 +1,36 @@
+let adquirirBtn = document.getElementById('adquirirBtn');
+
+adquirirBtn.addEventListener('click', function () {
+    window.location.href = '../Pages/contacto.html';
+});
+
+let adquirirBtn2 = document.getElementById('adquirirBtn2');
+
+adquirirBtn2.addEventListener('click', function () {
+    window.location.href = '../Pages/contacto.html';
+});
+
+let adquirirBtn3 = document.getElementById('adquirirBtn3');
+
+adquirirBtn3.addEventListener('click', function () {
+    window.location.href = '../Pages/contacto.html';
+});
+
+let adquirirBtn4 = document.getElementById('adquirirBtn4');
+
+adquirirBtn4.addEventListener('click', function () {
+    window.location.href = '../Pages/contacto.html';
+});
+
+let adquirirBtn5 = document.getElementById('adquirirBtn5');
+
+adquirirBtn5.addEventListener('click', function () {
+    window.location.href = '../Pages/contacto.html';
+});
+
+let adquirirBtn6 = document.getElementById('adquirirBtn6');
+
+adquirirBtn6.addEventListener('click', function () {
+    window.location.href = '../Pages/contacto.html';
+});
+
