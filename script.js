f2369fe944d2108af90e186ca50118160a0cc8aa
Matias Vazquez <mativazquezfotografia@hotmail.com>
2023-12-14 22:34:48 -0300

    prueba formulario

--- script.js
+++ script.js
@@ -34,3 +34,22 @@ adquirirBtn6.addEventListener('click', function () {
     window.location.href = '../Pages/contacto.html';
 });
 
+const boxes = document.querySelectorAll('.box')
+
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes()
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 5 * 4
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
+}
